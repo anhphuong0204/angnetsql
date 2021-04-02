76e93d50f20c749638b914115d14005a4cb6c4ea
anhphuong0204 <47697482+anhphuong0204@users.noreply.github.com>
2021-04-03 01:23:09 +0700

    Add CORS and repositories

---
+++ WebAPI/WebAPI/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public Department Department { get; set; }'
+        public DateTime JoinDate { get; set; }
+        public string PhotoFileDir { get; set; }
+    }
+}
